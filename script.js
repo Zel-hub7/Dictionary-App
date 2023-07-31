853a54963062302f01cc13f5fd8cac8e6acc99f0
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-31 01:55:16 -0700

    fetch API

--- script.js
+++ script.js
@@ -4,7 +4,29 @@ const sound = document.getElementById("sound");
 
 const btn = document.getElementById("search-btn");
 
-btn.addEventListener("click", ()=> {
-    let inpWord = document.getElementById('inp-word').value;
-    console.log(inpWord)
-})
+btn.addEventListener("click", () => {
+  let inpWord = document.getElementById("inp-word").value;
+  console.log(inpWord);
+  fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data[0].word);
+        console.log(data[0].meanings[0].definitions[0].definition)
+        result.innerHTML = `<div class="word">
+        <h3>${data[0].word}</h3>
+        <button>
+         <i class="fas fa-volume-up"></i>
+        </button>
+    </div>
+    <div class="details">
+        <p>pos</p>
+        <p>/Sample</p>
+    </div>
+    <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+    </p>
+    <p class="word-example">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, necessitatibus.
+    </p>`
+    });
+});
